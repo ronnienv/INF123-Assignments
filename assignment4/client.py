f705aff6436168bdd8d8d8fb10acac714534392d
ronnienv <itsjustronnie@yahoo.com>
2014-04-24 19:09:49 -0700

    yeee

--- assignment4/client.py
+++ assignment4/client.py
@@ -2,6 +2,7 @@ from network import Handler, poll
 import sys
 from threading import Thread
 from time import sleep
+import msvcrt
 
 
 myname = raw_input('What is your name? ')
@@ -9,14 +10,25 @@ myname = raw_input('What is your name? ')
 class Client(Handler):
     
     def on_close(self):
-        pass
+    	global running
+    	global sys
+        self.do_send({'user': myname, 'txt': 'quit'})
+        print "**** Disconnected From Server ****"
+        running = 0
+        raise SystemExit
+
     
     def on_msg(self, msg):
-        print msg
+    	if msg != "close":
+        	print msg
+        else:
+        	self.do_close()
+        	
         
 host, port = 'localhost', 8888
 client = Client(host, port)
 client.do_send({'join': myname})
+running = 1
 
 def periodic_poll():
     while 1:
@@ -27,6 +39,10 @@ thread = Thread(target=periodic_poll)
 thread.daemon = True  # die when the main thread dies 
 thread.start()
 
-while 1:
-    mytxt = sys.stdin.readline().rstrip()
-    client.do_send({'speak': myname, 'txt': mytxt})
+while running:
+	
+	mytxt = sys.stdin.readline()
+	if mytxt == "quit":
+		client.on_close()
+	else:
+		client.do_send({'speak': myname, 'txt': mytxt})
--- assignment4/server.py
+++ assignment4/server.py
@@ -1,7 +1,8 @@
 from network import Listener, Handler, poll
-
+import msvcrt
  
 handlers = {}  # map client handler to user name
+users_joined = ""
  
 class MyHandler(Handler):
      
@@ -9,13 +10,60 @@ class MyHandler(Handler):
         pass
          
     def on_close(self):
-        pass
+    	pass
      
     def on_msg(self, msg):
-        print msg
- 
+
+        if len(msg) == 1:
+        	add_client(self, msg)
+
+        elif msg['txt'] == "quit":
+        		remove_client(self, msg)
+
+    	else:
+    		for handler in handlers.values():
+        		if handler != self:
+        			handler.do_send(msg['speak'] + ": " + msg['txt'])
+
+def add_client(handler, msg):
+	
+	global users_joined
+	name = msg['join']
+	handlers[name] = handler
+
+	if users_joined == "":
+		users_joined = name
+	else:
+		users_joined = users_joined + ", " + name
+
+	for handler in handlers.values():
+		handler.do_send(msg['join'] + " joined. Users: " + users_joined)
+
+def remove_client(removed_handler, msg):
+	global users_joined
+
+	quiter = msg['user']
+	del handlers[quiter]
+	users_joined = ""
+
+	for name in handlers.keys():
+		if users_joined == "":
+			users_joined = name
+		else:
+			users_joined = users_joined + ", " + name
+
+	for handler in handlers.values():
+		if handler != removed_handler:
+			handler.do_send(quiter + " left the room. Users: "+ users_joined)
  
 port = 8888
 server = Listener(port, MyHandler)
-while 1:
-    poll(timeout=0.05) # in seconds
+running = 1
+while running:
+	try:
+		poll(timeout=0.05)
+	except KeyboardInterrupt:
+		print "**** Closing server ****"
+		for handler in handlers.values():
+			handler.do_send("close")
+		running = 0
