84b320d6c01dae2a36f3f5fa3b1e281782523575
ronnienv <itsjustronnie@yahoo.com>
2014-05-01 13:23:04 -0700

    Fixed Misunderstandings

--- assignment5/whalebot.py
+++ assignment5/whalebot.py
@@ -5,8 +5,6 @@ import common
 
 ################### CONTROLLER #############################
 
-import pygame
-from pygame.locals import KEYDOWN, QUIT, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT
 import common
 
 class Controller():
@@ -18,16 +16,6 @@ class Controller():
         # cmd_list = ["up","down","left","right"]
         # cmd = cmd_list[randint(0, 3)]
 
-        for event in pygame.event.get(): 
-            if event.type == QUIT:
-                cmd = 'quit'
-                self.m.do_cmd(cmd)
-            if event.type == KEYDOWN:
-                key = event.key
-                if key == K_ESCAPE:
-                    cmd = 'quit'
-                    self.m.do_cmd(cmd)
-                    
         self.m.get_pellet()
 
 ################### VIEW #############################
@@ -37,24 +25,11 @@ class View():
 
     def __init__(self, m):
         self.m = m
-        pygame.init()
-        self.screen = pygame.display.set_mode((400, 300))
         
     def display(self):
-        screen = self.screen
-        borders = [pygame.Rect(b[0], b[1], b[2], b[3]) for b in self.m.borders]
-        pellets = [pygame.Rect(p[0], p[1], p[2], p[3]) for p in self.m.pellets]
-        b = self.m.mybox
-        myrect = pygame.Rect(b[0], b[1], b[2], b[3])
-        screen.fill((0, 0, 64))  # dark blue
-        pygame.draw.rect(screen, (0, 191, 255), myrect)  # Deep Sky Blue
-        [pygame.draw.rect(screen, (255, 192, 203), p) for p in pellets]  # pink
-        [pygame.draw.rect(screen, (0, 191, 255), b) for b in borders]  # red
-        pygame.display.update()
-
         self.ticks+=1
         if self.ticks % 50 == 0:
-            print "Position: " + str(myrect.x) + ", " + str(myrect.y)
+            print "Position: " + str(self.m.mybox[0]) + ", " + str(self.m.mybox[1])
     
 ################### LOOP #############################
 
@@ -63,7 +38,11 @@ c = Controller(model)
 v = View(model)
 
 while not model.game_over:
-    sleep(0.02)
-    c.poll()
-    model.update()
-    v.display()
+    try:
+        sleep(0.02)
+        c.poll()
+        model.update()
+        v.display()
+    except KeyboardInterrupt:
+        print "****bot terminated***"
+        model.game_over = True
